7908387f62f7c494c8b94d0fbb6ec96a5186d0ec
Farhad87 <sabilov_farhad@mail.ru>
2019-10-30 21:21:48 +0300

    create get_message

--- bot.py
+++ bot.py
@@ -5,11 +5,22 @@ import config
 token = config.token
 URL = 'https://api.telegram.org/bot' + token + '/'
 
+
 def get_updates():
     url = URL + 'getupdates'
     print(url)
     r = requests.get(url)
     return r.json()
+
+
+def get_message():
+    data = get_updates()
+    chat_id = data['result'][-1]['message']['chat']['id']
+    message_text = data['result'][-1]['message']['text']
+    message = {'chat_id': chat_id,
+               'text': message_text}
+    return message
+
 
 def main():
     print(get_updates())
