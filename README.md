e5e2874e36c9b9212e3e387940d3bc64fbb5cc9d
Farhad87 <sabilov_farhad@mail.ru>
2019-11-05 19:52:48 +0300

    added route, get_photo, send_message to bot. Modified Readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Телеграмм бот
+[Телеграмм бот](https://github.com/Farhad87/telegramm_bot/blob/master/bot.py) - бот классификатор кошек/собак
--- bot.py
+++ bot.py
@@ -1,30 +1,44 @@
+from flask import Flask
+from flask import request
 import requests
-import config
+
+app = Flask(__name__)
 
 
-token = config.token
-URL = 'https://api.telegram.org/bot' + token + '/'
+def get_photo(photo_id, token):
+    url = 'https://api.telegram.org/bot' + token + '/' + 'getFile?file_id=' + str(photo_id)
+    r = requests.get(url).json()
+    path_to_photo = r['result']['file_path']
+    file_url = 'https://api.telegram.org/file/bot' + token + '/' + str(path_to_photo)
+    return requests.get(file_url)
 
 
-def get_updates():
-    url = URL + 'getupdates'
-    print(url)
-    r = requests.get(url)
-    return r.json()
+def send_message(token, chat_id, text='text'):
+    url = 'https://api.telegram.org/bot' + token + '/' + 'sendMessage' + '?chat_id=' + str(chat_id) + '&text=' + text
+    return requests.get(url)
 
 
-def get_message():
-    data = get_updates()
-    chat_id = data['result'][-1]['message']['chat']['id']
-    message_text = data['result'][-1]['message']['text']
-    message = {'chat_id': chat_id,
-               'text': message_text}
-    return message
-
-
-def main():
-    print(get_updates())
+@app.route('/<str:token>/', methods=['GET', 'POST'])
+def index(token):
+    if request.method == 'POST':
+        r = request.get_json()
+        chat_id = r['message']['chat']['id']
+        try:
+            message = r['message']['text']
+        except:
+            message = False
+        try:
+            photo_id = r['message']['photo'][-1]['file_id']
+        except:
+            photo_id = False
+        if message:
+            send_message(token, chat_id, text='I can classify cats and dogs, send me a photo, please (not a document)')
+        elif photo_id:
+            get_photo(photo_id, token)
+        else:
+            send_message(token, chat_id, text='I can classify cats and dogs, send me a photo, please (not a document)')
+    return '<h1>test server</h1>'
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
